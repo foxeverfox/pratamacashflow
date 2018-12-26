2c711e3e8a0d36753e3d5adeb9d5dc1e801063da
unknown <christianyt@KPNO-IT-04.pif.co.id>
2018-12-26 17:22:55 +0700

    fullfill confirmation

--- src/components/fundrequestlist.component.js
+++ src/components/fundrequestlist.component.js
@@ -64,6 +64,7 @@ function statusFormatter(x, _row) {
                 
                 this.toggle= this.toggle.bind(this);          
                 this.toggle2= this.toggle2.bind(this);          
+                this.toggle3= this.toggle3.bind(this);          
                 this.statusFormatter = statusFormatter.bind(this)
                 
                 this.doProses = this.doProses.bind(this);
@@ -72,7 +73,16 @@ function statusFormatter(x, _row) {
                 this.doPayConfirm = this.doPayConfirm.bind(this);
                 
                 }
+                
+                
+                onChange = (e) => {
 
+                    const state = this.state;
+            
+                    state[e.target.name] =e.target.value;
+                    this.setState(state);
+            
+                }
                 
 
                 onCollectionUpdate = (querySnapshot) => {
@@ -139,11 +149,41 @@ function statusFormatter(x, _row) {
 
 
                 doPay(){
-                    console.log('Pay')
-                }
+
+                    const id = this.state.ar_id
+
+                    
+                    let tgl = new Date;
+    
+                    
+                    const queryRef = firebase.firestore().collection('fundrequest').doc(id);    
+    
+                         queryRef.update({status_dokumen:3 ,
+                                        fullfill_date:this.state.fullfill_date,
+                                        fullfill_inputdate:tgl,
+                                        fullfill_note:this.state.fullfill_note ,
+                                        fullfill_voucher:this.state.fullfill_voucher     });
+
+                                
+                          }    
+                
                 
                 doPayConfirm(){
-                    console.log('Pay Confirm')
+
+                    const id = this.state.ar_id
+
+                    
+                    let tgl = new Date;
+    
+                    
+                    const queryRef = firebase.firestore().collection('fundrequest').doc(id);    
+    
+                         queryRef.update({status_dokumen:4 ,
+                                        fullfill_confirmdate:tgl,
+                                        });
+
+
+                    
                 }
 
                 doProses(){
@@ -159,9 +199,21 @@ function statusFormatter(x, _row) {
                         this.doPayConfirm()
                       }
                     this.setState({modal:false })
+                    this.setState({modal2:false })
+                    this.setState({modal3:false })
 
                  
-                 }   
+                 }  
+                 
+                 onChange = (e) => {
+
+                    const state = this.state;
+            
+                    state[e.target.name] =e.target.value;
+                    this.setState(state);
+            
+                }  
+
 
                 render() {
 
@@ -235,21 +287,17 @@ function statusFormatter(x, _row) {
                             const noinvoice = row.noinvoice ;
                             const status_dokumen=row.status_dokumen;
 
-
-                            
                             this.setState({ar_requestnote:request_note})
                             this.setState({ar_nominal:nominal})
                             this.setState({ar_tglrequest:tglrequest});
                             this.setState({ar_noinvoice:noinvoice});
                             this.setState({ar_statusdokumen:status_dokumen});
                             this.setState({ar_id:id});
-
-                           
-
-
                         }
                       };
 
+
+                    
                     return (
 
 
@@ -347,49 +395,46 @@ function statusFormatter(x, _row) {
                             
                         <Modal isOpen={this.state.modal2} toggle={this.toggle2} className={this.props.className}>
                                 <ModalHeader toggle={this.toggle2} className="modal-header">
-                                        <h4 className="modal-title">Pay to APL</h4>
+                                        <h2 className="modal-title">Proceed Payment to APL</h2>
                                 </ModalHeader>
                                 <ModalBody>
 
-                                    <div className="ibox">
+
+                                <div>PT Pratama Interdana Finance menyatakan telah mentransfer uang sebesar  </div>
+                                <div className='ibox text-center' >
+                                <h2> <span style={{color:'red',textAlign:'center'}}>{priceFormatter (this.state.ar_nominal )} </span> </h2>
+                                </div>
                                 
-                                         <div className="row">
-                                    
-                                            <div className="col-lg-6">
-                                                 <dl className="row mb-0">
-                                                    <div className="col-sm-4 text-sm-right">
-                                                      <dt>Tanggal</dt> 
-                                                    </div>
-                                                    <div className ='col-sm-8 text-sm-left'>
-                                                      {this.state.ar_tglrequest }
-                                                    </div>  
-                                                </dl>  
+                                <div className='ibox text-left' >
 
-                                                <dl className="row mb-0">
-                                                    <div className="col-sm-4 text-sm-right">
-                                                    <dt>NO B/L</dt> 
-                                                    </div>
-                                                    <div className ='col-sm-8 text-sm-left'>
-                                                    {this.state.ar_noinvoice }
-                                                    </div>  
-                                                </dl>  
+                                  <div className='form-group row'>
+                                    <label className="col-lg-4 col-form-label">Payment Date</label> 
 
-                                                <dl className="row mb-0">
-                                                    <div className="col-sm-4 text-sm-right">
-                                                    <dt>Nominal</dt> 
-                                                    </div>
-                                                    <div className ='col-sm-8 text-sm-left'>
-                                                    { priceFormatter(this.state.ar_nominal) }
-                                                    </div>  
-                                                </dl>   
-                                            </div>
-                                        </div>
+                                    <div class="col-lg-8">
+                                        <input className="form-control" name="fullfill_date" type="date"  onChange ={ this.onChange }/>
                                     </div>
+                                    
+
+
+                                  
+                                  </div>
+
+
+                                  
+
+                                  <label>Payment Voucher</label>
+                                  <input className="form-control" name="fullfill_voucher" type="text" onChange ={ this.onChange } />  
+
+                                  <label>Payment Note</label>  
+                                  <textarea class="form-control" name="fullfill_note" onChange ={ this.onChange } rows="3"></textarea>
+
+                                </div>
+                                        
 
                                  </ModalBody>
 
                                 <ModalFooter>
-                                    <Button color="primary" onClick={this.doProses}>Approve</Button>{' '}
+                                    <Button color="primary" onClick={this.doProses}>Proceed</Button>{' '}
                                     <Button color="secondary" data-dismiss="modal" onClick={this.toggle2}>Cancel</Button>
                                 </ModalFooter>
                             </Modal>
@@ -398,47 +443,18 @@ function statusFormatter(x, _row) {
                                 <ModalHeader toggle={this.toggle3} className="modal-header">
                                         <h4 className="modal-title">Payment Receive Confirmation</h4>
                                 </ModalHeader>
-                                <ModalBody>
+                                    <ModalBody>
 
-                                    <div className="ibox">
-                                
-                                         <div className="row">
-                                    
-                                            <div className="col-lg-6">
-                                                 <dl className="row mb-0">
-                                                    <div className="col-sm-4 text-sm-right">
-                                                      <dt>Tanggal</dt> 
-                                                    </div>
-                                                    <div className ='col-sm-8 text-sm-left'>
-                                                      {this.state.ar_tglrequest }
-                                                    </div>  
-                                                </dl>  
-
-                                                <dl className="row mb-0">
-                                                    <div className="col-sm-4 text-sm-right">
-                                                    <dt>NO B/L</dt> 
-                                                    </div>
-                                                    <div className ='col-sm-8 text-sm-left'>
-                                                    {this.state.ar_noinvoice }
-                                                    </div>  
-                                                </dl>  
-
-                                                <dl className="row mb-0">
-                                                    <div className="col-sm-4 text-sm-right">
-                                                    <dt>Nominal</dt> 
-                                                    </div>
-                                                    <div className ='col-sm-8 text-sm-left'>
-                                                    { priceFormatter(this.state.ar_nominal) }
-                                                    </div>  
-                                                </dl>   
-                                            </div>
+                                        <div>PT APL  menyatakan telah menerima uang sebesar  </div>
+                                        <div className='ibox text-center' >
+                                        <h2> <span style={{color:'red',textAlign:'center'}}>{priceFormatter (this.state.ar_nominal )} </span> </h2>
                                         </div>
-                                    </div>
 
-                                 </ModalBody>
+                                               
 
+                                    </ModalBody>
                                 <ModalFooter>
-                                    <Button color="primary" onClick={this.doProses}>Approve</Button>{' '}
+                                    <Button color="primary" onClick={this.doProses}>Confirm</Button>{' '}
                                     <Button color="secondary" data-dismiss="modal" onClick={this.toggle3}>Cancel</Button>
                                 </ModalFooter>
                             </Modal>
